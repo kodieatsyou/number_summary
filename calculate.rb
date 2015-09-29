5badde854a01c3daae46d1ce1a4c60b1f9215fce
Naoki Yahashi <yahashinj@s.dcsdk12.org>
2015-09-29 09:39:13 -0600

    Stuff

--- calculate.rb
+++ calculate.rb
@@ -3,6 +3,10 @@ require_relative 'number_summary'
 a = (1..6).map { |i| i }
 print a, "\n"
 
-print NumberSummary.mode(a), "\n"
+print NumberSummary.mean(a), "\n"
 
-puts NumberSummary.summarize('data.csv')
+array = NumberSummary.summarize('data.csv')
+puts NumberSummary.min(array)
+puts NumberSummary.max(array)
+puts NumberSummary.mean(array)
+puts NumberSummary.median(array)
